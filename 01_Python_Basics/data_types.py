dfcda9fd80bffe6e05b048bd3f7f7c4385af34ec
Md Farhan Sadik Udoy <udoysadik@gmail.com>
2026-07-08 23:10:54 +0600

    Writting data type

---
+++ 01_Python_Basics/data_types.py
@@ -0,0 +1,19 @@
+## Integer Data Type
+print("Here are some Integer Data type")
+age = 20  
+marks = 95
+temperature = -3
+
+print(age)
+print(marks)
+print(temperature)
+
+#Float data type
+print("Here are some Float Data type")
+cgpa = 3.80
+price = 250.50
+height = 5.9
+
+print(cgpa)
+print(price)
+print(height)
