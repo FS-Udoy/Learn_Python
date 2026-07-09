9ae22e3bbe7e9983ca958dade779b6862e53525b
Md Farhan Sadik Udoy <udoysadik@gmail.com>
2026-07-09 23:46:15 +0600

    Type casting

---
+++ 01_Python_Basics/type_casting.py
@@ -0,0 +1,15 @@
+#Though Input return String we need to type casting whatever we need data type
+
+#age =input("Enter your age: ")
+#print(age+5)
+# its will showing error because string+int not doing sum
+
+#INT
+age =int(input("Enter your age: "))
+print(age+5)
+
+#FLOAT
+cgpa=float(input("Enter your cgpa: "))
+print(cgpa)
+
+
