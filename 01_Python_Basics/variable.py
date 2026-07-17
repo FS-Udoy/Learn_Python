1a3768bb9b3188d9a961f18b21717dfe70d2635b
Md Farhan Sadik Udoy <udoysadik@gmail.com>
2026-07-18 01:36:29 +0600

    add more variable

--- 01_Python_Basics/variable.py
+++ 01_Python_Basics/variable.py
@@ -2,3 +2,11 @@ name="Md Farhan Sadik Udoy"
 print(name)
 university="Daffodil International University"
 print(university)
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius=diameter/2
+
+print(radius)
